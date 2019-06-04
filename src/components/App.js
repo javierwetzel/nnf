a6ae5cc0200742e3e2b8d77296f9b7fd726b7c17
EnzoEsper <enzoesper@hotmail.com>
2019-06-04 18:25:53 -0300

    Componente App para renderear pag

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div id="app">
+      <div id="wrapper">
+        <h1 className="glitch" data-text="Error 404: NOMBRE NOT FOUND">Error 404: NOMBRE NOT FOUND</h1>
+        <span class="sub">PRUEBA IC</span>
+      </div>
+    </div>   
+  );
+};
+
+export default App;
